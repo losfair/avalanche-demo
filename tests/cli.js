9da85dd92593fe72c1c3bad5e9361e484f64f15e
losfair <zhy20000919@hotmail.com>
2018-12-02 23:17:51 +0800

    Detect consensus failure

--- tests/cli.js
+++ tests/cli.js
@@ -2,16 +2,17 @@ const network = require("../build/network");
 const slush = require("../build/slush");
 const snowflake = require("../build/snowflake");
 
-const N_NODES = 100;
+const N_NODES = 1000;
 
 class UserContext {
     constructor(name, byzantine) {
         this.name = name;
         this.color = null;
         this.byzantine = byzantine;
-        this.sampleSize = 10;
+        this.sampleSize = 20;
         this.alpha = 0.7;
-        this.beta = 5;
+        this.beta = 15;
+        this.messageCount = 0;
     }
 
     updateColor(color) {
@@ -58,7 +59,15 @@ setInterval(() => {
         console.log("All nodes are red.");
     }
     if (all_decided) {
-        console.log("All nodes are decided in " + n_iter + " iterations.");
+        if (!all_red) {
+            console.log("All nodes are decided but some of them are not red - consensus failed!");
+            process.exit(1);
+        }
+        let messageCount = 0;
+        for (let n of net.nodes) {
+            messageCount += n.config.userContext.messageCount;
+        }
+        console.log("All " + net.nodes.length + " nodes are decided in " + n_iter + " iterations with a total of " + messageCount + " messages.");
         process.exit(0);
     }
 }, 100);
